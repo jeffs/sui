bf938941cef4123c992d84d3cd04b4fd5b47faae
MW Tian <mingwei@mystenlabs.com>
2025-05-09 13:36:52 +0900

    update

--- consensus/core/src/commit.rs
+++ consensus/core/src/commit.rs
@@ -511,7 +511,11 @@ impl Display for LeaderStatus {
 /// Decision of each leader slot.
 #[derive(Debug, Clone, PartialEq)]
 pub(crate) enum DecidedLeader {
+    /// The committed leader block and whether it is a direct commit.
+    /// It is incorrect to trigger the direct commit optimization when the commit is not.
+    /// So when it is unknown if the commit is direct, the boolean flag should be false.
     Commit(VerifiedBlock, bool),
+    /// The skipped leader slot where no block is committed.
     Skip(Slot),
 }
 
